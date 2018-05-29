31e7a1a5e9f993995725e71a84c6b5226a96d0d3
zjh <1429823270@qq.com>
2018-05-29 09:53:41 +0800

    zQuery的初始化

--- zQuery-0.0.1.js
+++ zQuery-0.0.1.js
@@ -5,18 +5,18 @@
   var jQuery = function (selector,context) {
     return new jQuery.prototype.init(selector,context);
   };
+
   // 02 设置jQuery的原型对象
   jQuery.fn = jQuery.prototype = {
     constructor : jQuery,
-    getName : function(){
-
-    },
     init :function(selector,context){
-      this.Name = selector;
-      this.age = context;
+      //......
     }
   };
+
   // 03 设置init构造函数的原型对象为jquery的原型对象
   jQuery.fn.init.prototype = jQuery.fn;
 
-})(window)
+  // 04 把函数作用域内的jQuery绑定给全局对象window
+  window.jQuery = window.$ = jQuery;
+})(window);

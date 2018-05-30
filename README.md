8bd4cae5546370f940f91bf7b5bddbc73fe8cddc
zjh <1429823270@qq.com>
2018-05-30 22:23:12 +0800

    update

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ zQuery0.0.1 工厂函数的实现,初始化init方法<br>
 zQuery0.0.2 绑定jQuery为window对象的方法,对传给jQuery的值类型为false的处理<br>
 zQuery0.0.3 值类型为string并且是标签字符串的处理<br>
 zQuery0.0.4 工具类的抽取,字符串空格字段的判断<br>
-zQuery0.0.5 工具类的抽取,字符串空格字段的判断<br>
+zQuery0.0.5 传选择器字符串的处理,以及部分重复代码的优化<br>
+zQuery0.0.6 传选择器字符串的处理,以及部分重复代码的优化<br>
--- zQuery-0.0.6.js
+++ zQuery-0.0.6.js
@@ -2,7 +2,6 @@
 (function (window,undefined) {
   // 01 创建工厂函数
   var jQuery = function (selector) {
-    // 返回init的实例对象
     return new jQuery.fn.init(selector)
   };
   // 02 设置jQuery的原型对象
@@ -10,26 +9,27 @@
     constructor : jQuery,
     init : function (selector) {
       /**
-       * 0 类型为false
-       * 1 类型为字符串(选择器,标签代码)
-       * 2 数组
-       * 3 伪数组
-       * 4 对象,数字
-       * 5 函数
+       * 判断传进来的参数
+       * 0 类型为false 返回空实例
+       * 1 类型为字符串(选择器,标签) 返回传进来的标签或选择器选中的DOM
+       * 2 类型为数组
+       * 3 类型为伪数组
+       * 4 类型为对象,数字
+       * 5 类型为函数
        */
-      // 类型为false
+      // 判断类型是否false
       if (!selector) {
         return this
       }
-      // 类型为字符串
+      // 判断是否是字符串
       else if (tools.isString(selector)) {
-        // 判断是否为标签元素
+        // 判断是否是标签元素
         if (tools.isHTML(selector)) {
           var oDiv = document.createElement('div').innerHTML = selector;
-          [].push.apply(this,oDiv.children);
+          [].push.apply(this,oDiv);
           return this
         }
-        // 不是标签元素就是选择器
+        // 不是标签就是选择器
         else {
           var obj = document.querySelectorAll(selector);
           [].push.apply(this,obj);
@@ -38,15 +38,15 @@
       }
     }
   };
-  // 03 设置init的原型对象为jQuery的原型对象
+  // 03 设置init的原型对象为jQuery
   jQuery.fn.init.prototype = jQuery.fn;
-  // 04 绑定jQuery给window对象的方法
+  // 04 把jQuery绑定为window对象的方法
   window.jQuery = window.$ = jQuery;
 
   //工具类
   var tools = {
     /**
-     * 判断是否是字符串
+     * 判断是否为字符串
      * @param string
      * @returns {boolean}
      */
@@ -54,7 +54,7 @@
       return typeof string === 'string'
     },
     /**
-     * 判断是否是标签元素
+     * 判断是否为标签元素
      * @param string
      * @returns {boolean}
      */
@@ -63,16 +63,16 @@
       return str.charAt(0) === '<' && str.charAt(str.length - 1) === '>' && str.length >= 3
     },
     /**
-     * 去除字符串中开头结尾的空格
+     * 去除字符串开头结尾的空格
      * @param string
      * @returns {*}
      */
     trim : function (string) {
       if (string.trim) {
         return string.trim()
-      }else {
+      } else {
         return string.replace(/^\s+|\s+$/g,'')
       }
     }
   }
-})(window);
+})(window)
